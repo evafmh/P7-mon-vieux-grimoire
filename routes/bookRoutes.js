0fa00e53a0b31a552a4739de3caf149bdf52e424
Eva F <eva.famechon@outlook.com>
2023-05-25 22:22:22 +0200

    Implement token-based authentication Secure routes with middleware auth.js

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const auth = require("../middleware/auth");
 const bookController = require("../controllers/bookController");
 
 // Créer un livre
-router.post("/", bookController.createBook);
+router.post("/", auth, bookController.createBook);
 
 // Obtenir la liste des livres
 router.get("/", bookController.getAllBooks);
@@ -13,9 +14,9 @@ router.get("/", bookController.getAllBooks);
 router.get("/:id", bookController.getBookById);
 
 // Mettre à jour un livre
-router.put("/:id", bookController.updateBook);
+router.put("/:id", auth, bookController.updateBook);
 
 // Supprimer un livre
-router.delete("/:id", bookController.deleteBook);
+router.delete("/:id", auth, bookController.deleteBook);
 
 module.exports = router;
