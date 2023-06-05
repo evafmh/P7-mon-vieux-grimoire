8e5bec83e15c4bdd7f10fbd007e3421978c6f348
Eva F <eva.famechon@outlook.com>
2023-06-05 11:11:52 +0200

    Add user and password verification

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,29 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
+// Regex pour les formats d'emails et de mots de passe
+const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordFormat = /^(?=.*\d).{8,}$/;
+
 // Fonction pour créer un nouvel utilisateur
 exports.signup = (req, res) => {
+    const errorMessages = [];
+
+    // Vérifie le format de l'email
+    if (!emailFormat.test(req.body.email)) {
+        errorMessages.push("Format de l'email invalide.");
+    }
+    // Vérifie le format du mot de passe
+    if (!passwordFormat.test(req.body.password)) {
+        errorMessages.push(
+            "Format du mot de passe invalide. Le mot de passe doit contenir au moins 8 caractères et au moins 1 chiffre."
+        );
+    }
+    // Renvoie l'erreur
+    if (errorMessages.length > 0) {
+        return res.status(400).json({ messages: errorMessages });
+    }
+
     // Logique pour créer un nouvel utilisateur
     bcrypt
         .hash(req.body.password, 10)
@@ -48,7 +69,19 @@ exports.login = (req, res) => {
                         ),
                     });
                 })
-                .catch((error) => res.status(500).json({ error }));
+                .catch((error) => {
+                    if (error instanceof bcrypt.BCryptError) {
+                        return res.status(401).json({
+                            message:
+                                "Erreur lors de la comparaison des mots de passe",
+                        });
+                    } else {
+                        return res.status(500).json({
+                            message:
+                                "Une erreur est survenue lors de la connexion",
+                        });
+                    }
+                });
         })
         .catch((error) => res.status(500).json({ error }));
 };
